64bba3d1491d5b57d64253411970041c3f60f591
LohainyOliveira <102706685+LohainyOliveira@users.noreply.github.com>
2022-11-18 01:08:37 -0300

    Add files via upload

---
+++ 404/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ERRO 404</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"/> 
+</head>
+<body>
+    <div class="container">
+        <h2>Ops! Página não encontrada.</h2>
+        <img href="imagem/fundo.jpg"><h1>404</h1></img>
+        <p>Não conseguimos encontrar a página que você está procurando.</p>
+        <a href=#>Volte para página de início!</a>
+    </div>
+    
+</body>
+</html>
